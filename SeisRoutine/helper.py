fead5f0df99838afb122de1dd00ce0534bb2b515
ikahbasi <ikahbasi@gmail.com>
2024-08-30 11:15:35 +0330

    I wrote a function named clean_nordic_catalog to clean the earthquake catalog in Nordic format from lines containing unwanted characters.

--- SeisRoutine/helper.py
+++ SeisRoutine/helper.py
@@ -18,3 +18,28 @@ def read_gcf(path: str):
         except Exception as error:
             print('Error', fname, error)
     return st
+
+
+def clean_nordic_catalog(inp_name, out_name,
+    skip_list=['FOCMEC', 'BIN', 'IAML', 'ACTION', 'GAP']):
+    '''
+    Cleanup earthquake catalog in nordic format.
+    '''
+    with open(inp_name, 'r') as inp_file:
+        with open(out_name, 'w') as out_file:
+            for line in inp_file:
+                # If the line is empty.
+                if line.strip() == '':
+                    continue
+                if '-' not in line:
+                    continue
+                # If specific character exist in the line.
+                skip = False
+                for char in skip_list:
+                    if char in line:
+                        skip = True
+                        break
+                if skip:
+                    continue
+                # Writing in the output file.
+                out_file.write(line)
