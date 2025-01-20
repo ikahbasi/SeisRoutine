a4ac23f778434392d0ca2d73d9d727d2dd81909a
Iman Kahbasi <ikahbasi@gmail.com>
2025-01-20 15:53:30 +0330

    A function to configure log settings
    A function to configure log settings

--- SeisRoutine/config.py
+++ SeisRoutine/config.py
@@ -1,4 +1,6 @@
 import yaml
+import logging
+
 
 class Config:
     def __init__(self, **entries):
@@ -17,3 +19,41 @@ def load_config(file_path):
     with open(file_path, 'r') as file:
         config_dict = yaml.safe_load(file)
         return Config(**config_dict)
+
+def configure_logging(level,
+                      log_format='%(asctime)s - %(levelname)s - %(message)s',
+                      mode='console',
+                      filename='app.log'):
+    """
+    Configure logging settings based on mode.
+
+    Parameters:
+    level (str): Logging level.
+    mode (str): Mode of logging - 'console', 'file', or 'both'.
+    filename (str): The filename for logging to a file (if needed).
+    """
+    numeric_level = getattr(logging, level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError(f'Invalid log level: {level}')
+        
+    logger = logging.getLogger()
+    logger.setLevel(numeric_level)
+    if logger.hasHandlers():
+        logger.handlers.clear()
+    
+    if mode == 'console' or mode == 'both':
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(numeric_level)
+        console_handler.setFormatter(logging.Formatter(log_format))
+        logger.addHandler(console_handler)
+    
+    if mode == 'file' or mode == 'both':
+        file_handler = logging.FileHandler(filename)
+        file_handler.setLevel(numeric_level)
+        file_handler.setFormatter(logging.Formatter(log_format))
+        logger.addHandler(file_handler)
+    
+    logger.propagate = False
+    for name in logging.root.manager.loggerDict.keys():
+        if name not in ('my_module', '__main__'):
+            logging.getLogger(name).setLevel(logging.WARNING)
