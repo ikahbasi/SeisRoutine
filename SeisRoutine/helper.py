6c294f6fedba3a38947c76102e22f981330bc657
Iman Kahbasi <ikahbasi@gmail.com>
2025-01-27 18:47:32 +0330

    The read_gcf function was moved to the waveform module.
    The read_gcf function was moved to the waveform module.

--- SeisRoutine/helper.py
+++ SeisRoutine/helper.py
@@ -3,23 +3,6 @@ from obspy import Stream
 import glob
 
 
-def read_gcf(path: str):
-    '''
-    There is a problem in some .gcf files that cause an error
-    when we try to read them with ObsPy.
-    
-    Docs ???
-    '''
-    lst = glob.glob(path)
-    st = Stream()
-    for fname in lst:
-        try:
-            st += read(fname)
-        except Exception as error:
-            print('Error', fname, error)
-    return st
-
-
 def clean_nordic_catalog(inp_name, out_name,
     skip_list=['FOCMEC', 'BIN', 'IAML', 'ACTION', 'GAP']):
     '''
